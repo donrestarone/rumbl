44eed93661af8a402ac13228b1b9ad155e54116f
donrestarone <shashikejayatunge@gmail.com>
2020-12-26 17:21:17 -0500

    add logic for user form, using a changeset to create a user and display validation errors

--- lib/rumbl_web/router.ex
+++ lib/rumbl_web/router.ex
@@ -18,7 +18,7 @@ defmodule RumblWeb.Router do
 
     get "/", PageController, :index
 
-    resources "/users", UserController, only: [:index, :show]
+    resources "/users", UserController, only: [:index, :show, :new, :create]
   end
 
   # Other scopes may use custom stacks.
