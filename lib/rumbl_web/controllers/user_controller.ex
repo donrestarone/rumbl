df7e6e3e79a8c93a25fe94b1197a414aeb50a9c6
donrestarone <shashikejayatunge@gmail.com>
2020-12-26 20:41:56 -0500

    add hashed passwords + implement registration changeset

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -6,7 +6,7 @@ defmodule RumblWeb.UserController do
   alias Rumbl.Repo
 
   def new(conn, _params) do
-    changeset = Accounts.change_user(%User{})
+    changeset = Accounts.change_registration(%User{}, %{})
     render(conn, "new.html", changeset: changeset)
   end
 
@@ -21,7 +21,7 @@ defmodule RumblWeb.UserController do
   end
 
   def create(conn, %{"user" => user_params}) do
-    case Accounts.create_user(user_params) do
+    case Accounts.register_user(user_params) do
       {:ok, user} ->
         conn
         |> put_flash(:info, "#{user.name} created!")
